3d48258abae4119f0fb960f1f5e4ec50f34f1234
cristian-cll <78435266+cristian-cll@users.noreply.github.com>
2021-05-14 21:43:43 +0200

    second modification

---
+++ js/data.js
@@ -0,0 +1,166 @@
+//1.recivimos los datos
+
+let array=[
+    { 
+  company:"Manage",
+  new:"New!",
+  featured:"Featured",
+  job:"Fullstack Developer",
+  day:"1d ago",
+  turn:"Part Time",
+  work:"Remote",
+  role:"Fullstack",
+  level:"Midweight",
+  languages:["Python", "JavaScript", "Java"],
+  tools:"React"
+},
+{ 
+    company:"Jose",
+    new:"New!",
+    featured:"Featured",
+    job:"junior Developer",
+    day:"4d jul",
+    turn:"Part Time",
+    work:"Remote",
+    role:"Fullstack",
+    level:"Midweight",
+    languages:["Python", "JavaScript", "JOse",  "C"],
+    tools:"React"
+  }
+]
+
+/* 
+  <!-- Item Start -->
+  Account
+  New!
+  Junior Frontend Developer
+  2d ago
+  Part Time
+  USA only
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  JavaScript
+  <!-- Tools -->
+  React
+  Sass
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  MyHome
+  Junior Frontend Developer
+  5d ago
+  Contract
+  USA only
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  CSS
+  JavaScript
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  Loop Studios
+  Software Engineer
+  1w ago
+  Full Time
+  Worldwide
+  <!-- Role -->
+  Fullstack
+  <!-- Level -->
+  Midweight
+  <!-- Languages -->
+  JavaScript
+  Ruby
+  <!-- Tools -->
+  Sass
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  FaceIt
+  Junior Backend Developer
+  2w ago
+  Full Time
+  UK only
+  <!-- Role -->
+  Backend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  Ruby
+  <!-- Tools -->
+  RoR
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  Shortly
+  Junior Developer
+  2w ago
+  Full Time
+  Worldwide
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  HTML
+  JavaScript
+  <!-- Tools -->
+  Sass
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  Insure
+  Junior Frontend Developer
+  2w ago
+  Full Time
+  USA only
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  JavaScript
+  <!-- Tools -->
+  Vue
+  Sass
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  Eyecam Co.
+  Full Stack Engineer
+  3w ago
+  Full Time
+  Worldwide
+  <!-- Role -->
+  Fullstack
+  <!-- Level -->
+  Midweight
+  <!-- Languages -->
+  JavaScript
+  Python
+  <!-- Tools -->
+  Django
+  <!-- Item End -->
+
+  <!-- Item Start -->
+  The Air Filter Company
+  Front-end Dev
+  1mo ago
+  Part Time
+  Worldwide
+  <!-- Role -->
+  Frontend
+  <!-- Level -->
+  Junior
+  <!-- Languages -->
+  JavaScript
+  <!-- Tools -->
+  React
+  Sass
+  <!-- Item End -->
+} */
